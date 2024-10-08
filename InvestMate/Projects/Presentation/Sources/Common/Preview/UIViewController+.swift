caf55219219afff2d9ebc5c9dcc140e24bae2db9
joho2022 <josama2022.dev@gmail.com>
2024-10-08 23:49:24 +0900

    ✨ KAN-11: UIViewController 미리보기 지원

---
+++ InvestMate/Projects/Presentation/Sources/Common/Preview/UIViewController+.swift
@@ -0,0 +1,27 @@
+//
+//  UIViewController+.swift
+//  InvestMate
+//
+//  Created by 조호근 on 10/7/24.
+//
+
+#if DEBUG
+import SwiftUI
+
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+            let viewController: UIViewController
+
+            func makeUIViewController(context: Context) -> UIViewController {
+                return viewController
+            }
+
+            func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            }
+        }
+
+        func toPreview() -> some View {
+            Preview(viewController: self)
+        }
+}
+#endif
