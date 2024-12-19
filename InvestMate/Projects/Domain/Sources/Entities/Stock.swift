b178b8545ca63805564e80f8088641debd2c8b3f
joho2022 <josama1997@naver.com>
2024-12-20 01:38:33 +0900

    ✨ KAN-23: 종목관리 Domain 기본 구조 구현

---
+++ InvestMate/Projects/Domain/Sources/Entities/Stock.swift
@@ -0,0 +1,32 @@
+//
+//  Stock.swift
+//  StockTests
+//
+//  Created by 조호근 on 12/13/24.
+//
+
+import Foundation
+
+public struct Stock: Equatable, Identifiable {
+    
+    public let id: UUID
+    public let name: String
+    public let averagePrice: Double
+    public let quantity: Double
+    public let totalPrice: Double
+    
+    public init(
+        id: UUID = UUID(),
+        name: String,
+        averagePrice: Double,
+        quantity: Double,
+        totalPrice: Double
+    ) {
+        self.id = id
+        self.name = name
+        self.averagePrice = averagePrice
+        self.quantity = quantity
+        self.totalPrice = totalPrice
+    }
+    
+}
